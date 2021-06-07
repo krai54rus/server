3176249f174a0524f26ba76d6d0ff3748e5b6ee1
krai54rus <krai54rus@gmail.com>
2021-06-07 12:37:39 +0700

    qwe

--- server/routes/disks.js
+++ server/routes/disks.js
@@ -1,17 +1,25 @@
 module.exports = function(app,db) {
     app.get('/disks', function(req,res){
       const collection = db.db("portal").collection("beer");
-      collection.find({}).toArray(function(err, beers){
+      collection.find({}).toArray(function(err, disks){
           if(err) return console.log(err);
-          res.send(beers);
+          res.send(disks);
       });
     });
     app.get('/disks/all', function(req,res){
       const collection = db.db("diplom").collection("disks");
-      collection.find({}).toArray(function(err, beers){
+      collection.find({}).toArray(function(err, disks){
           if(err) return console.log(err);
           console.log('/disks/all');
-          res.send(beers);
+          res.send(disks);
+      });
+    });
+    app.get('/personal/disks', function(req,res){
+      const collection = db.db("diplom").collection("disks");
+      collection.find({}).limit( 5 ).toArray(function(err, disks){
+          if(err) return console.log(err);
+          console.log('/personal/disks');
+          res.send(disks);
       });
     });
     app.get('/beer/style', function(req,res){
