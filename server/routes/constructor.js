9a6c4cdcbc807e94a74db2ec3db6f60f9cca24cf
eushakov@umg-marketing.com <eushakov@umg-marketing.com>
2021-06-08 16:24:35 +0700

    constructor find disks

--- server/routes/constructor.js
+++ server/routes/constructor.js
@@ -6,27 +6,20 @@ module.exports = function(app,db) {
       const model = req.query.model;
       let diskSize = '';
       if (marka) {
-        //
+        // Поиск марки
         let resMarka = collAuto.findOne({name:marka},function(err, markaObj){
             if(err) return console.log(err);
-            console.log(marka);
             if (markaObj) {
+              // Поиск модели марки и ее размеров
               const resModel = markaObj.models.filter(modelObj=> modelObj.name == model);
-              console.log(resModel[0]);
               diskSize = resModel[0].size;
-              console.log(diskSize);
             }
-            collDisks.find({size: diskSize}).toArray(function(err, disks){
+            // Поиск дисков по размерам модели
+            collDisks.find( { size: { $in: diskSize } } ).toArray(function(err, disks){
               if(err) return console.log(err);
-              //console.log(disks);
               res.send(disks);
           });
         });
       }
-      // res.send(resMarka);
-      // collection.find({}).toArray(function(err, disks){
-      //     if(err) return console.log(err);
-      //     res.send(disks);
-      // });
     });
   };
